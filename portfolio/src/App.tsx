fecb7ad4b704d2fa934cea0b5ef7dedef2e3e901
Your Name <you@example.com>
2025-12-02 14:54:47 +0100

    Init++

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
